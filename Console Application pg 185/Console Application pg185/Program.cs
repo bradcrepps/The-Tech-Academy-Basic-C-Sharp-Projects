137448dea5602bdbc5647c1729a7868b8b0324cd
BradCrepps <77355802+bradcrepps@users.noreply.github.com>
2021-06-04 12:50:30 -0700

    Math Comparison Assig.

---
+++ Console Application pg 185/Console Application pg185/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Application_pg185
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("What number would you like to perfrom calculations on? ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(input + " + 2 = " + Operations.addTwo(input));
+            Console.WriteLine(input + " - 2 = " + Operations.subTwo(input));
+            Console.WriteLine(input + " / 2 = " + Operations.divTwo(input));
+
+            Console.Read();
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Math and Comparison Operator Assignment/Math and Comparison Operator Assignment/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_and_Comparison_Operator_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Person 1
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.WriteLine("Person 1: ");
+            Console.WriteLine("What is person1's Hourly Rate?");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("How many hours does Person1 work per week?");
+            int userInput2 = Convert.ToInt32(Console.ReadLine());
+
+            //Person 2
+            Console.WriteLine("Person 2: ");
+            Console.WriteLine("What is person's 2 hourly Rate");
+            int userInput3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("How many hours does person2 work per week?");
+            int userInput4 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The annual salary of Person 1 is:");
+            int income1 = userInput * userInput2 * 52;
+            Console.WriteLine(income1);
+            Console.WriteLine("The annual salary of Person 2 is:");
+            int income2 = userInput3 * userInput4 * 52;
+            Console.WriteLine(income2);
+
+            //Boolean check
+            Console.WriteLine("Does Person 1 make more money than Person 2?");
+            int person1 = income1;
+            int person2 = income2;
+            bool value = person1 > person2;
+            Console.WriteLine(value);
+            Console.ReadLine();
+
+
+        }
+    }
+}
